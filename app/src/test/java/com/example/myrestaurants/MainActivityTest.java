7c5012fd648a2f773a5f8fda7de8a7e6432f1d2b
Dun-Njuguna <duncankariuki60@gmail.com>
2019-03-05 14:44:36 +0300

    used yelp api

---
+++ app/src/test/java/com/example/myrestaurants/MainActivityTest.java
@@ -0,0 +1,43 @@
+package com.example.myrestaurants;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import com.example.myrestaurants.ui.MainActivity;
+import com.example.myrestaurants.ui.RestaurantListActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent(){
+        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+        assertEquals(true,"MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted(){
+        activity.findViewById(R.id.findRestaurantsButton).performClick();
+        Intent expectedIntent = new Intent(activity, RestaurantListActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
